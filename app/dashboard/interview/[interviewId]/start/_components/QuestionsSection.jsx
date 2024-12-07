e62fa2b1b548a0990b16e62ba76ded8c027ddaaa
Varshith B A <140150143+Varshithkulal@users.noreply.github.com>
2024-12-07 20:37:57 +0530

    Update QuestionsSection.jsx

--- app/dashboard/interview/[interviewId]/start/_components/QuestionsSection.jsx
+++ app/dashboard/interview/[interviewId]/start/_components/QuestionsSection.jsx
@@ -32,7 +32,7 @@ function QuestionsSection({mockInterviewQuestion,activeQuestionIndex}) {
                 <strong>Note:</strong>
             </h2>
             <h2 className='text-sm text-red-700 my-2'>{process.env.NEXT_PUBLIC_QUESTION_NOTE}</h2>
-          <h2 className='text-sm mt-3 flex gap-2 items-center text-red-700'><strong>Disclaimer</strong></h2>
+            <h2 className='text-sm mt-3 flex gap-2 items-center text-red-700'><strong>Disclaimer</strong></h2>
             <p className='text-sm flex gap-2 items-center text-red-700'>Please make sure to enable the audio before answering the question.</p>
         </div>
     </div>
